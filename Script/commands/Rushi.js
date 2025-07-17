c28214d60cc43c66655f8dd37724a2302b96d77a
butterfly0727 <chrisevanx6@gmail.com>
2025-07-17 18:59:54 +0600

    Update Rushi.js

--- Script/commands/Rushi.js
+++ Script/commands/Rushi.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rushia",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "Butterfly Sizu💟🦋 & Maruf System💫",
  description: "Random Rushia",
  commandCategory: "random-img",
  usages: "rushia",

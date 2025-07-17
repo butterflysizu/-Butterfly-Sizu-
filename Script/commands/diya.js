8d8dc61fde5615aa0568ebb33ece0e343466b9a0
butterfly0727 <chrisevanx6@gmail.com>
2025-07-17 16:06:58 +0600

    Update diya.js

--- Script/commands/diya.js
+++ Script/commands/diya.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "doya",
  version: "1.2.8",
  hasPermssion: 0,
- credits: "Islamick Chat", //Don't chinge The credit
+ credits: "💫Butterfly🦋 Sizu💟", //Don't chinge The credit
  description: "Short doya",
  commandCategory: "doya",
  usages: "doya",
